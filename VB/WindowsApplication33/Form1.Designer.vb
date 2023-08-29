ccd468fad97bb3e0f33c5b21986370c0002b68ff
DevExpressExampleBot <fakeEmail>
2023-08-29 12:24:10 +0400

    Source auto update [skip ci]

--- VB/WindowsApplication33/Form1.Designer.vb
+++ VB/WindowsApplication33/Form1.Designer.vb
@@ -1,83 +1,82 @@
-Imports Microsoft.VisualBasic
-Imports System
 Namespace WindowsApplication33
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.components = New System.ComponentModel.Container()
-			Dim resources As New System.ComponentModel.ComponentResourceManager(GetType(Form1))
-			Me.imageComboBoxEdit1 = New DevExpress.XtraEditors.ImageComboBoxEdit()
-			Me.imageList1 = New System.Windows.Forms.ImageList(Me.components)
-			Me.imageList2 = New System.Windows.Forms.ImageList(Me.components)
-			CType(Me.imageComboBoxEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' imageComboBoxEdit1
-			' 
-			Me.imageComboBoxEdit1.Location = New System.Drawing.Point(12, 12)
-			Me.imageComboBoxEdit1.Name = "imageComboBoxEdit1"
-			Me.imageComboBoxEdit1.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() { New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
-			Me.imageComboBoxEdit1.Size = New System.Drawing.Size(186, 20)
-			Me.imageComboBoxEdit1.TabIndex = 0
-			' 
-			' imageList1
-			' 
-			Me.imageList1.ImageStream = (CType(resources.GetObject("imageList1.ImageStream"), System.Windows.Forms.ImageListStreamer))
-			Me.imageList1.TransparentColor = System.Drawing.Color.Fuchsia
-			Me.imageList1.Images.SetKeyName(0, "arrowleft_green16.bmp")
-			Me.imageList1.Images.SetKeyName(1, "arrowright_green16.bmp")
-			Me.imageList1.Images.SetKeyName(2, "save16.bmp")
-			Me.imageList1.Images.SetKeyName(3, "stop16.bmp")
-			' 
-			' imageList2
-			' 
-			Me.imageList2.ImageStream = (CType(resources.GetObject("imageList2.ImageStream"), System.Windows.Forms.ImageListStreamer))
-			Me.imageList2.TransparentColor = System.Drawing.Color.Fuchsia
-			Me.imageList2.Images.SetKeyName(0, "arrowleft_green_32.bmp")
-			Me.imageList2.Images.SetKeyName(1, "arrowright_green_32.bmp")
-			Me.imageList2.Images.SetKeyName(2, "save_32.bmp")
-			Me.imageList2.Images.SetKeyName(3, "stop_32.bmp")
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(485, 317)
-			Me.Controls.Add(Me.imageComboBoxEdit1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-'			Me.Load += New System.EventHandler(Me.Form1_Load);
-			CType(Me.imageComboBoxEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(WindowsApplication33.Form1))
+            Me.imageComboBoxEdit1 = New DevExpress.XtraEditors.ImageComboBoxEdit()
+            Me.imageList1 = New System.Windows.Forms.ImageList(Me.components)
+            Me.imageList2 = New System.Windows.Forms.ImageList(Me.components)
+            CType((Me.imageComboBoxEdit1.Properties), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' imageComboBoxEdit1
+            ' 
+            Me.imageComboBoxEdit1.Location = New System.Drawing.Point(12, 12)
+            Me.imageComboBoxEdit1.Name = "imageComboBoxEdit1"
+            Me.imageComboBoxEdit1.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+            Me.imageComboBoxEdit1.Size = New System.Drawing.Size(186, 20)
+            Me.imageComboBoxEdit1.TabIndex = 0
+            ' 
+            ' imageList1
+            ' 
+            Me.imageList1.ImageStream = CType((resources.GetObject("imageList1.ImageStream")), System.Windows.Forms.ImageListStreamer)
+            Me.imageList1.TransparentColor = System.Drawing.Color.Fuchsia
+            Me.imageList1.Images.SetKeyName(0, "arrowleft_green16.bmp")
+            Me.imageList1.Images.SetKeyName(1, "arrowright_green16.bmp")
+            Me.imageList1.Images.SetKeyName(2, "save16.bmp")
+            Me.imageList1.Images.SetKeyName(3, "stop16.bmp")
+            ' 
+            ' imageList2
+            ' 
+            Me.imageList2.ImageStream = CType((resources.GetObject("imageList2.ImageStream")), System.Windows.Forms.ImageListStreamer)
+            Me.imageList2.TransparentColor = System.Drawing.Color.Fuchsia
+            Me.imageList2.Images.SetKeyName(0, "arrowleft_green_32.bmp")
+            Me.imageList2.Images.SetKeyName(1, "arrowright_green_32.bmp")
+            Me.imageList2.Images.SetKeyName(2, "save_32.bmp")
+            Me.imageList2.Images.SetKeyName(3, "stop_32.bmp")
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(485, 317)
+            Me.Controls.Add(Me.imageComboBoxEdit1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((Me.imageComboBoxEdit1.Properties), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private imageComboBoxEdit1 As DevExpress.XtraEditors.ImageComboBoxEdit
-		Private imageList1 As System.Windows.Forms.ImageList
-		Private imageList2 As System.Windows.Forms.ImageList
-	End Class
+#End Region
+        Private imageComboBoxEdit1 As DevExpress.XtraEditors.ImageComboBoxEdit
+
+        Private imageList1 As System.Windows.Forms.ImageList
+
+        Private imageList2 As System.Windows.Forms.ImageList
+    End Class
 End Namespace
-
--- VB/WindowsApplication33/Form1.vb
+++ VB/WindowsApplication33/Form1.vb
@@ -1,60 +1,58 @@
-Imports Microsoft.VisualBasic
 Imports System
 Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
 Imports DevExpress.XtraEditors.Controls
 Imports DevExpress.XtraEditors
 
 Namespace WindowsApplication33
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
+    Public Partial Class Form1
+        Inherits Form
 
-		Private disabledItems As New List(Of String)(New String() {"Left", "Right"})
-		Dim selectionCancelled As Boolean = False
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-			imageComboBoxEdit1.Properties.SmallImages = imageList1
-			imageComboBoxEdit1.Properties.LargeImages = imageList2
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-			imageComboBoxEdit1.Properties.Items.Add(New ImageComboBoxItem("Left", 0))
-			imageComboBoxEdit1.Properties.Items.Add(New ImageComboBoxItem("Right", 1))
-			imageComboBoxEdit1.Properties.Items.Add(New ImageComboBoxItem("Save", 2))
-			imageComboBoxEdit1.Properties.Items.Add(New ImageComboBoxItem("Cancel", 3))
+        Private disabledItems As List(Of String) = New List(Of String)(New String() {"Left", "Right"})
 
-			AddHandler imageComboBoxEdit1.Properties.DrawItem, AddressOf Properties_DrawItem
-			AddHandler imageComboBoxEdit1.Properties.QueryCloseUp, AddressOf Properties_QueryCloseUp
-			AddHandler imageComboBoxEdit1.Properties.CloseUp, AddressOf Properties_CloseUp
+        Private selectionCancelled As Boolean = False
 
-			Dim i As New ImageComboBoxItem()
-		End Sub
-		Private Sub Properties_CloseUp(ByVal sender As Object, ByVal e As CloseUpEventArgs)
-			e.AcceptValue = Not selectionCancelled
-		End Sub
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            imageComboBoxEdit1.Properties.SmallImages = imageList1
+            imageComboBoxEdit1.Properties.LargeImages = imageList2
+            imageComboBoxEdit1.Properties.Items.Add(New ImageComboBoxItem("Left", 0))
+            imageComboBoxEdit1.Properties.Items.Add(New ImageComboBoxItem("Right", 1))
+            imageComboBoxEdit1.Properties.Items.Add(New ImageComboBoxItem("Save", 2))
+            imageComboBoxEdit1.Properties.Items.Add(New ImageComboBoxItem("Cancel", 3))
+            AddHandler imageComboBoxEdit1.Properties.DrawItem, New ListBoxDrawItemEventHandler(AddressOf Properties_DrawItem)
+            AddHandler imageComboBoxEdit1.Properties.CloseUp, New CloseUpEventHandler(AddressOf Properties_CloseUp)
+            AddHandler imageComboBoxEdit1.Properties.QueryCloseUp, New CancelEventHandler(AddressOf Properties_QueryCloseUp)
+            Dim i As ImageComboBoxItem = New ImageComboBoxItem()
+        End Sub
 
-		Private Sub Properties_QueryCloseUp(ByVal sender As Object, ByVal e As CancelEventArgs)
-			Dim edit As ImageComboBoxEdit = TryCast(sender, ImageComboBoxEdit)
-			If edit.GetPopupEditForm() IsNot Nothing Then
-				Dim selectedItemIndex As Integer = edit.GetPopupEditForm().SelectedItemIndex
-				Dim selectedItem As String = edit.Properties.Items(selectedItemIndex).ToString()
-				If disabledItems.Contains(selectedItem) Then
-					e.Cancel = True
-				End If
-			End If
+        Private Sub Properties_CloseUp(ByVal sender As Object, ByVal e As CloseUpEventArgs)
+            e.AcceptValue = Not selectionCancelled
+        End Sub
 
-			selectionCancelled = e.Cancel
-		End Sub
+        Private Sub Properties_QueryCloseUp(ByVal sender As Object, ByVal e As CancelEventArgs)
+            Dim edit As ImageComboBoxEdit = TryCast(sender, ImageComboBoxEdit)
+            If edit.GetPopupEditForm() IsNot Nothing Then
+                Dim selectedItemIndex As Integer = edit.GetPopupEditForm().SelectedItemIndex
+                Dim selectedItem As String = edit.Properties.Items(selectedItemIndex).ToString()
+                If disabledItems.Contains(selectedItem) Then
+                    e.Cancel = True
+                End If
+            End If
 
-		Private Sub Properties_DrawItem(ByVal sender As Object, ByVal e As DevExpress.XtraEditors.ListBoxDrawItemEventArgs)
-			If disabledItems.Contains(e.Item.ToString()) Then
-				e.Appearance.ForeColor = Color.Gray
-			End If
-		End Sub
-	End Class
+            selectionCancelled = e.Cancel
+        End Sub
+
+        Private Sub Properties_DrawItem(ByVal sender As Object, ByVal e As ListBoxDrawItemEventArgs)
+            If disabledItems.Contains(e.Item.ToString()) Then
+                e.Appearance.ForeColor = Color.Gray
+            End If
+        End Sub
+    End Class
 End Namespace
